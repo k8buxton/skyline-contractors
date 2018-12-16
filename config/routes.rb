7c7958af33704b78dca68b9ca90c740171534eff
Kate Buxton <k8hunter@hotmail.com>
2018-12-16 11:07:29 +0000

    new rocksolid project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'services', to: 'pages#services', as: :services
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
